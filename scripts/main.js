d574f50cabc2e81b1265ffb663efc92e7b5fe06d
«Danica» <usuari@domini.com>
2026-04-16 16:36:39 +0200

    lol

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,9 @@
 const miTitulo = document.querySelector("h1");
 miTitulo.textContent = "¡Hola mundo!";
+
+let helado = "chocolate";
+if (helado === "chocolate") {
+  alert("¡Sí, amo el helado de chocolate!");
+} else {
+  alert("Awwww, pero mi favorito es el de chocolate...");
+}
